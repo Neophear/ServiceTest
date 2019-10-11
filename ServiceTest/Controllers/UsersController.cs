a414635d98049cfbfd6991060f12e85d21677aa5
Neophear <stiiggade@gmail.com>
2019-10-11 10:42:03 +0200

    Initial commit

---
+++ ServiceTest/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using ServiceTest.Entities;
+using ServiceTest.Services;
+
+namespace ServiceTest.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
